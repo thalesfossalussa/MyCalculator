317b6c3f70d0f344e92978fdb5c9ab425b1821da
Thales Fossalussa <thalesvitor2001@gmail.com>
2020-10-27 18:13:27 -0300

    Adicionado os tipos de operação: Potenciação e Raiz Quadrada

--- TesteInicial/Program.cs
+++ TesteInicial/Program.cs
@@ -51,6 +51,24 @@ namespace TesteInicial
             Console.WriteLine("\nA divisão dos números é: " + div.ToString("N2") + "\n");
         }
 
+        static void potenciacao()
+        {
+            double x, expoente;
+            Console.Write("Escreva o número: ");
+            x = leituraInt();
+            Console.Write("Agora escreva o expoente: ");
+            expoente = leituraInt();
+            Console.WriteLine("O resultado da potenciação é: " + Math.Pow(x,expoente) + "\n");
+        }
+
+        static void raizQuadrada()
+        {
+            double x;
+            Console.Write("Escreva o valor da raiz: ");
+            x = leituraInt();
+            Console.WriteLine("O resultado da raiz quadrada é: " + Math.Sqrt(x) + "\n");
+        }
+
         static void Main(string[] args)
         {
             int menu;
@@ -68,6 +86,8 @@ namespace TesteInicial
                 Console.WriteLine("2 - Multiplicação");
                 Console.WriteLine("3 - Subtração");
                 Console.WriteLine("4 - Divisão");
+                Console.WriteLine("5 - Potenciação");
+                Console.WriteLine("6 - Raiz quadrada");
                 Console.WriteLine("0 - Sair");
 
                 menu = leituraInt();
@@ -87,6 +107,12 @@ namespace TesteInicial
                     case 4:
                         divisao();
                             break;
+                    case 5:
+                        potenciacao();
+                        break;
+                    case 6:
+                        raizQuadrada();
+                        break;
                     default:
                         Console.WriteLine("\nValor inválido, por favor tente novamente!\n");
                         break;
